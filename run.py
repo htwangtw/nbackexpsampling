58c8c98acc45e2914fadff45da61f97556294f80
Hao Ting Wang <hw1012@york.ac.uk>
2017-08-10 13:09:37 +0100

    fix typos

--- run.py
+++ run.py
@@ -55,7 +55,8 @@ if __name__ == "__main__":
     fixation = fixation_cross(window=Experiment.window, color='black')
     stimulus = responsescreen(window=Experiment.window)
     switch = Text(window=Experiment.window, text='Switch', color='black')    
-
+    endtxt = open('./instructions/end_instr.txt', 'r').read().split('#\n')[0]
+    end_msg = Text(window=Experiment.window, text=endtxt, color='black')
     # generate trials
     Experiment.trials= next(trial_generator)
 
@@ -99,10 +100,7 @@ if __name__ == "__main__":
         respRT = None
     
     # ending message
-    endtxt = open('./instructions/end_instr.txt', 'r').read().split('#\n')[0]
-    msgTxT = Text(window=Experiment.window, text=endtxt, color='black')
-    msgTxt.setText(endtxt)
-    msgTxt.draw()
+    end_msg.draw()
     win.flip()
     core.wait(1)
 
