6232db41c2025b280e7ec5aa47f0eb60418d9491
Hao-Ting Wang <haoting.wang@york.ac.uk>
2017-11-07 17:41:31 +0000

    fixed wait keys at the end

--- run.py
+++ run.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
     # ending message
     end_msg.draw()
     Experiment.window.flip()
-    event.waitKeys('return')
+    event.waitKeys(keyList=['return'])
 
     logging.flush()
     # change output files to read only
